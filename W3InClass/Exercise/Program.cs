08b9a6343871806aad37afae1aae8da4f805c2ef
Aycan Feyzulla <aycanikom@abv.bg>
2018-10-16 15:49:38 +0300

    Exercise

--- W3InClass/Exercise/Program.cs
+++ W3InClass/Exercise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,33 @@ namespace Exercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Do you want to play some music?");
+            string answer = Console.ReadLine();
+            if (answer=="Yes")
+            {
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\asus\Desktop\W3\PU_IntroCSharp_InClass_1801681035\W3InClass\Exercise\Music\09 Kamikaze.wav"))
+                {
+                    Console.Write("Now playing Eminem-Kamikaze");
+                    player.PlaySync();
+                    
+                }
+            }
+            else if(answer == "Fuck")
+            {
+                Console.WriteLine("Yes, fuck you too!!!");
+            }
+            else if (answer=="No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong imput!");
+            }
+
+           
+
+            
         }
     }
 }
